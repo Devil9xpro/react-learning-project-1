53793b9fed6f4ff85c23a38bd8b5906b2878b3e2
David Nguye <chinhzz3107@gmail.com>
2022-03-08 18:42:23 +0700

    add first component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import ExpenseItem from "./components/ExpenseItem";
+
 function App() {
-  return (
-      <div>
-        <h2>Let's get started!</h2>
-      </div>
-  );
+    return (<div>
+        <h1>Let's get started!</h1>
+        <ExpenseItem/>
+    </div>);
 }
 
 export default App;
